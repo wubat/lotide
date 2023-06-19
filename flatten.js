688be688217683cfe3e9ee94b23e4a465f56cfe0
james <jameswu000@gmail.com>
2023-06-18 22:41:58 -0600

    flatten nested array in array

--- flatten.js
+++ flatten.js
@@ -16,3 +16,26 @@ function assertArraysEqual(arr1, arr2) {
     console.log(`💔💔💔 Assertion Failed`)
   }
 }
+
+function flatten(arr) {
+  let newArray = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        newArray.push(arr[i][j])
+      }
+    }
+      else {
+        newArray.push(arr[i])
+      }
+    }
+    console.log(newArray)
+}
+  
+
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+
+// const testArray = [1, [6, 7, 8], 2, 3, 4, 5]
+// console.log(Array.isArray(testArray[1][1]))
