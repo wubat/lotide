81674a5fa06d26e3919035614809b9217c2172bd
james <jameswu000@gmail.com>
2023-07-30 13:28:26 -0600

    added missing functions files and updateeed version for republish

---
+++ assertObjectsEqual.js
@@ -0,0 +1,13 @@
+
+const eqObjects = require('./eqObjects');
+
+const assertObjectsEqual = function(object1, object2) { 
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2) === true) { 
+    console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`); 
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
+
+module.exports = assertObjectsEqual;
---
+++ eqObjects.js
@@ -0,0 +1,23 @@
+const eqObjects = function(obj1, obj2) {
+
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+module.exports = eqObjects;
